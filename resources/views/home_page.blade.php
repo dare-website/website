16552580faf5e844c9123d44e19acdf37dcbd7b1
davidlgabriel <david.g.leal00@gmail.com>
2022-10-25 16:32:56 +0100

    first commit

---
+++ resources/views/home_page.blade.php
@@ -0,0 +1,163 @@
+@extends('layout')
+
+@section('content')
+<section class="video-container">
+    <video src="video/Web.mp4" autoplay loop playsinline muted></video>
+</section>
+
+
+<div class="container" style="margin-top:5rem;">
+
+    <section>
+        <div>
+            <h3 style="font-family: 'Adam-Medium';font-weight:bold; text-align:center;margin-bottom:3rem;">O SEU
+                PARCEIRO PERFEITO PARA O SEU NEGOCIO</h3>
+        </div>
+
+        <div class="row" style="text-align: center;">
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto">
+                <h1 style="color:#f9c656;margin-bottom:0.8rem"><i class="fi-cwluhl-clock-wide"></i></h1>
+                <h4>djndcdfncjd</h4>
+                <h6>dshcbhdshcbhdshcbhdshcbh dshcbhdshcbhdshcbh</h6>
+            </div>
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto">
+                <h1 style="color:#d73588;margin-bottom:0.8rem"><i class="fi-cwluhl-clock-wide"></i></h1>
+                <h4>djndcdfncjd</h4>
+                <h6>dshcbhdshcbhdshcbhdshcbh dshcbhdshcbhdshcbh</h6>
+            </div>
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto">
+                <h1 style="color:#0ea29a;margin-bottom:0.8rem"><i class="fi-cwluhl-clock-wide"></i></h1>
+                <h4>djndcdfncjd</h4>
+                <h6>dshcbhdshcbhdshcbhdshcbh dshcbhdshcbhdshcbh</h6>
+            </div>
+
+        </div>
+    </section>
+
+    <section style="margin-top: 5rem;">
+       
+        <div class="row justify-content-center  d-flex align-items-center">
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto">
+                <h5 style="font-family: 'Adam-Light';">Somos especialistas em surpreender.</h5>
+                <h5 style="font-family: 'Adam-Medium';text-align:justify;">A nossa equipa é composta por cientistas, gastrónomos e criativos que se juntam para proporcionar uma
+EXPERIENCIA INOVADORA que irá mexer com os SEUS SENTIDOS.</h5>
+            </div>
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto">
+                
+                <h5 style="font-family: 'Adam-Medium';text-align:justify;">Trazendo ao mercado a melhor selecção de produtos diferenciadores, arrojados e vanguardistas, com o intuito de surpreender quem não está à espera de ser surpreendido.</h5>
+                <h5 style="font-family: 'Adam-Light';">Temos o complemento perfeito para a sua criatividade.</h5>
+            </div>
+            <div class="col-md-4 col-lg-3 col-xl-3 justify-content-center  d-flex align-items-center"
+                style="width:22rem !important;height:18rem !important; background: linear-gradient(50deg, rgba(249,198,86,1) 0%, rgba(252,162,103,1) 14%, rgba(202,40,65,1) 35%, rgba(215,53,136,1) 55%, rgba(77,40,93,1) 80%, rgba(14,162,154,1) 100%);">
+                <div>
+                    <h2 style="color:white;font-family: 'Adam-Medium';">DREAM</h2>
+                    <h2 style="color:white;font-family: 'Adam-Medium';">AMAZE</h2>
+                    <h2 style="color:white;font-family: 'Adam-Medium';">REPEAT</h2>
+                </div>
+            </div>
+        </div>
+
+
+    </section>
+    <section>
+        <div>
+            <h3 style="font-family: 'Adam-Medium';font-weight:bold; text-align:center;margin-bottom:3rem;margin-top:5rem;">IDEIAS</h3>
+        </div>
+        <div class="row">
+            <div class="gallery">
+                <figure>
+                    <img src="https://images.unsplash.com/photo-1448814100339-234df1d4005d?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600"
+                        alt="" />
+                    <figcaption>Daytona Beach <small>United States</small></figcaption>
+                </figure>
+                <figure>
+                    <img src="https://images.unsplash.com/photo-1443890923422-7819ed4101c0?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600"
+                        alt="" />
+                    <figcaption>Териберка, gorod Severomorsk <small>Russia</small></figcaption>
+                </figure>
+                <figure>
+                    <img src="https://images.unsplash.com/photo-1445964047600-cdbdb873673d?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600"
+                        alt="" />
+                    <figcaption>
+                        Bad Pyrmont <small>Deutschland</small>
+                    </figcaption>
+                </figure>
+                <figure>
+                    <img src="https://images.unsplash.com/photo-1439798060585-62ab242d7724?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600"
+                        alt="" />
+                    <figcaption>Yellowstone National Park <small>United States</small></figcaption>
+                </figure>
+                <figure>
+                    <img src="https://images.unsplash.com/photo-1440339738560-7ea831bf5244?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600"
+                        alt="" />
+                    <figcaption>Quiraing, Portree <small>United Kingdom</small></figcaption>
+                </figure>
+                <figure>
+                    <img src="https://images.unsplash.com/photo-1441906363162-903afd0d3d52?crop=entropy&fit=crop&fm=jpg&h=400&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=600"
+                        alt="" />
+                    <figcaption>Highlands <small>United States</small></figcaption>
+                </figure>
+                
+            </div>
+
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="display:none;">
+                <symbol id="close" viewBox="0 0 18 18">
+                    <path fill-rule="evenodd" clip-rule="evenodd" fill="#FFFFFF" d="M9,0.493C4.302,0.493,0.493,4.302,0.493,9S4.302,17.507,9,17.507
+			S17.507,13.698,17.507,9S13.698,0.493,9,0.493z M12.491,11.491c0.292,0.296,0.292,0.773,0,1.068c-0.293,0.295-0.767,0.295-1.059,0
+			l-2.435-2.457L6.564,12.56c-0.292,0.295-0.766,0.295-1.058,0c-0.292-0.295-0.292-0.772,0-1.068L7.94,9.035L5.435,6.507
+			c-0.292-0.295-0.292-0.773,0-1.068c0.293-0.295,0.766-0.295,1.059,0l2.504,2.528l2.505-2.528c0.292-0.295,0.767-0.295,1.059,0
+			s0.292,0.773,0,1.068l-2.505,2.528L12.491,11.491z" />
+                </symbol>
+            </svg>
+            <script>
+            popup = {
+                init: function() {
+                    $('figure').click(function() {
+                        popup.open($(this));
+                    });
+
+                    $(document).on('click', '.popup img', function() {
+                        return false;
+                    }).on('click', '.popup', function() {
+                        popup.close();
+                    })
+                },
+                open: function($figure) {
+                    $('.gallery').addClass('pop');
+                    $popup = $('<div class="popup" />').appendTo($('body'));
+                    $fig = $figure.clone().appendTo($('.popup'));
+                    $bg = $('<div class="bg" />').appendTo($('.popup'));
+                    $close = $('<div class="close"><svg><use xlink:href="#close"></use></svg></div>')
+                        .appendTo(
+                            $fig);
+                    $shadow = $('<div class="shadow" />').appendTo($fig);
+                    src = $('img', $fig).attr('src');
+                    $shadow.css({
+                        backgroundImage: 'url(' + src + ')'
+                    });
+                    $bg.css({
+                        backgroundImage: 'url(' + src + ')'
+                    });
+                    setTimeout(function() {
+                        $('.popup').addClass('pop');
+                    }, 10);
+                },
+                close: function() {
+                    $('.gallery, .popup').removeClass('pop');
+                    setTimeout(function() {
+                        $('.popup').remove()
+                    }, 100);
+                }
+            }
+
+            popup.init()
+            </script>
+          
+        </div>
+    </section>
+
+</div>
+
+@endsection
+
+
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>DARE</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <script defer src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js'></script>
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css'>
+
+    <script defer src="https://friconix.com/cdn/friconix.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.js"></script>
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<style>
+.nav-item {
+    margin-right: 5rem;
+}
+
+.dropdown-menu a:hover {
+    color: #000 !important;
+}
+
+.dropdown-menu a:active {
+    color: #fff !important;
+    background: linear-gradient(90deg, rgba(249, 198, 86, 1) 0%, rgba(252, 162, 103, 1) 14%, rgba(202, 40, 65, 1) 35%, rgba(215, 53, 136, 1) 55%, rgba(77, 40, 93, 1) 80%, rgba(14, 162, 154, 1) 100%);
+}
+</style>
+
+<body class="antialiased">
+    <script>
+    < script >
+        $(document).ready(function() {
+
+            $('ul.navbar-nav > li')
+                .click(function(e) {
+                    $('ul.navbar-nav > li')
+                        .removeClass('active');
+                    $(this).addClass('active');
+                });
+        });
+    </script>
+    </script>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a style="margin-left:7rem;" class="navbar-brand" href="#"><img style="width:6rem;"
+                    src="img/dare-horizontal-logo.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarNavDropdown">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-ltr " href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-ltr" href="#">Sobre Nós</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link nav-link-ltr dropdown-toggle" href="/produtos" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="true">
+                            Produtos
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="/produtos">ESFERIFICACAO</a></li>
+                            <li><a class="dropdown-item" href="#">LOLLIPOPS</a></li>
+                            <li><a class="dropdown-item" href="#">CANAPES</a></li>
+                            <li><a class="dropdown-item" href="#">SEMI-ACABADOS</a></li>
+                            <li><a class="dropdown-item" href="#">CITRICOS EXOTICOS</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-ltr" href="#">Contactos</a>
+                    </li>
+                    <!---->
+                </ul>
+            </div>
+        </div>
+
+
+
+    </nav>
+
+    <main>
+        @yield('content')
+    </main>
+    <!--<footer class="footer-16371">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-9 text-center">
+                        <div class="footer-site-logo mb-4">
+                            <a href="#"><img style="width:12rem" src="img/footer-dare.png" alt=""></a>
+                        </div>
+                        <ul class="list-unstyled nav-links mb-5">
+                            <li><a href="#">Menu</a></li>
+                            <li><a href="#">Sobre Nós</a></li>
+                            <li><a href="#">Produtos</a></li>
+                            <li><a href="#">Contactos</a></li>
+                        </ul>
+
+                        <div class="social mb-4">
+                            <h3>Stay in touch</h3>
+                            <ul class="list-unstyled">
+                                <li class="in"><a href="#"><i class="fi-xnsuxl-instagram"></i></a></li>
+                                <li class="fb"><a href="#"><i class="fi-xnsuxl-facebook"></i></a></li>
+                                <li class="tw"><a href="#"><i class="fi-xnsuxl-twitter-solid"></i></a></li>
+                            </ul>
+                        </div>
+
+                        <div class="copyright">
+                            <p class="mb-0"><small>&copy; Inoveit. All Rights Reserved.</small></p>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </footer>-->
+    <footer class="text-center text-lg-start text-white footer-16371"
+        style="background-color: #fffff; margin-top:5rem;">
+        <!-- Grid container -->
+        <div class="container pb-0">
+            <!-- Section: Links -->
+            <section class="">
+                <!--Grid row-->
+                <div class="row">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3 ">
+                        <div class="mb-2">
+                            <a href="#"><img style="width:12rem" src="img/footer-dare.png" alt=""></a>
+                        </div>
+                        <p style="font-family: 'Adam-Medium';" class="text-black">
+                            Somos especialistas em surpreender.
+                        </p>
+                        <p style="font-family: 'Adam-Medium';color:black;"><i class="fi-xnluxl-envelope"></i><a
+                                style="text-decoration:none; color:black !important" href="mailto:filipa.cruz@dare.pt">
+                                FILIPA.CRUZ@DARE.PT
+                            </a></p>
+                        <p style="font-family: 'Adam-Medium';color:black;"><i class="fi-xnlrxl-phone"></i> <a
+                                style="text-decoration:none; color:black !important" href="tel:+351913497009">+351 913
+                                497 009</a></p>
+
+                        <a style="color: #df588a !important" href="#"><i class="fi-xnsuxl-instagram"></i></a>
+                        <a style="color: #3742d0 !important" href="#"><i class="fi-xnsuxl-facebook"></i></a>
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="w-100 clearfix d-md-none" />
+
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold text-black"
+                            style="font-family: 'Adam-Medium';font-weight:bold;">PRODUTOS</h6>
+                        <p style="font-family: 'Adam-Medium';">
+                            <a style="text-decoration:none" class="text-black">ESFERIFICACAO</a>
+                        </p>
+                        <p style="font-family: 'Adam-Medium';">
+                            <a style="text-decoration:none" class="text-black"> LOLLIPOPS</a>
+                        </p>
+                        <p style="font-family: 'Adam-Medium';">
+                            <a style="text-decoration:none" class="text-black">CANAPES</a>
+                        </p>
+                        <p style="font-family: 'Adam-Medium';">
+                            <a style="text-decoration:none" class="text-black">SEMI-ACABADOS</a>
+                        </p>
+                        <p style="font-family: 'Adam-Medium';">
+                            <a style="text-decoration:none" class="text-black">CITRICOS EXOTICOS</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="w-100 clearfix d-md-none" />
+
+                    <!-- Grid column -->
+                    <hr class="w-100 clearfix d-md-none" />
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3 text-black footer-newsletter">
+                        <h4>Our Newsletter</h4>
+                        <p>Melhorar NEWSLETTER NÃO ACABADO, MAS FICA A IDEIA</p>
+                        <form action="" method="post">
+                            <input type="email" name="email"><input type="submit" value="Subscribe">
+                        </form>
+
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!--Grid row-->
+            </section>
+            <!-- Section: Links -->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center text-white p-3 "
+            style="margin-top:0.9rem; background: linear-gradient(90deg, rgba(249,198,86,1) 0%, rgba(252,162,103,1) 14%, rgba(202,40,65,1) 35%, rgba(215,53,136,1) 55%, rgba(77,40,93,1) 80%, rgba(14,162,154,1) 100%);">
+            © 2020 Copyright
+            <a class="text-white" href="#">inoveit.pt</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+
+</body>
+
+</html>
