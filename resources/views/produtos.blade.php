c1d5762352bf850f8acd353a1abbe81e9236d045
davidlgabriel <david.g.leal00@gmail.com>
2022-12-15 19:17:27 +0000

    j

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -37,8 +37,8 @@
     </div>
     <div class="portfolio-menu mt-2 mb-4">
         <ul class="linha" style="border-bottom:2px solid rgba(77,40,93,0.4);padding-right: 4rem;">
-            <li class="btn1 " data-filter=".esf">ESFERIFICACAO</li>
-            <li class="btn1 active" data-filter=".lolli">LOLLIPOPS</li>
+            <li class="btn1 active" data-filter=".esf">ESFERIFICACAO</li>
+            <li class="btn1 " data-filter=".lolli">LOLLIPOPS</li>
             <li class="btn1  text" data-filter=".cana">CANAPES</li>
             <li class="btn1  text" data-filter=".semi">SEMI-ACABADOS</li>
         </ul>
