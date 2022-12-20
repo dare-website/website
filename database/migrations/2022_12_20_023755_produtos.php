29f8f56391430b65e16ff0b8922570d05b5bf00e
davidlgabriel <david.g.leal00@gmail.com>
2022-12-20 02:39:57 +0000

    j

---
+++ database/migrations/2022_12_20_023755_produtos.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('id_tipo');
+            $table->string('avatar');
+            $table->string('alcool');
+            $table->string('texto');
+            $table->string('preparacao');
+            $table->string('formato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
