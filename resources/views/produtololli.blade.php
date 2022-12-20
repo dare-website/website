30e605057377c564ddf411e00f20eab2d18ee806
davidlgabriel <david.g.leal00@gmail.com>
2022-12-20 04:22:50 +0000

    d

--- resources/views/produtololli.blade.php
+++ resources/views/produtololli.blade.php
@@ -115,7 +115,7 @@
             <div class="portfolio-i row d-flex justify-content-center">
                 @foreach ($produtosR as $prodR)
                 <div class="ite prod0 col-md-3" style="text-align:center;">
-                    <a href="{{ route('produto', $prodR->id_tipo) }}" style="text-decoration:none !important;">
+                    <a href="<?php if($prodR->id_tipo == 3 || $prodR->id_tipo == 4){ route('produto', $prodR->id); }else{  route('produto', $prodR->id_tipo) ; }?>" style="text-decoration:none !important;">
                         <img class="img-fluid shadow" style="border-radius:15px;"
                             src="https://dare.pt/storage/{{$prodR->avatar}}" alt="">
                         <h6
