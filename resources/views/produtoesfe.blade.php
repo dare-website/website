ac9301aa8f85a046d39e6fe1b46839c9a83b519e
davidlgabriel <david.g.leal00@gmail.com>
2022-12-20 05:04:51 +0000

    d

--- resources/views/produtoesfe.blade.php
+++ resources/views/produtoesfe.blade.php
@@ -7,8 +7,8 @@
 <link href="{{ asset('css/style-produto.css') }}" rel="stylesheet">
 <section>
 
-    <div class="container" style="margin-top:5rem; margin-bottom:5rem;">
-        <div class="row" style="margin-top:2rem;">
+    <div class="container" style="margin-top:5.5rem; margin-bottom:5rem;">
+        <div class="row">
             <div class="col-lg-12 text-center my-2">
                 <div class="row">
                     <div class="col-md-6" style="text-align: left;">
