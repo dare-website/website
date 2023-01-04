5146709a892ba88525a7abd6c2e61ccf9f2c78e8
davidlgabriel <david.g.leal00@gmail.com>
2023-01-04 03:04:20 +0000

    d

--- resources/views/home_page.blade.php
+++ resources/views/home_page.blade.php
@@ -24,7 +24,7 @@
 
 @media(max-width: 449px) {
     .oh{
-        font-size:2rem;
+        font-size:2rem !important;
     }
     .versa1{
         background-image: url('img/dare-gif-responsive.gif') !important;
